eaafcb5d8b6b74e79482e2e0959ae492e9f64524
aaqib-yfz <aaqibshaheer2001@gmail.com>
2024-08-17 19:39:48 +0500

    test2

---
+++ cypress/e2e/locators.cy.js
@@ -0,0 +1,9 @@
+describe('suite', ()=>{
+it( 'locs test', ()=>{
+    cy.visit("https://www.amazon.com/")
+    cy.get('input#twotabsearchtextbox').type("T-Shirts")
+    cy.get('input#nav-search-submit-button').click()
+    cy.get('span.a-size-base.a-color-secondary.a-text-normal').contains('product') //assertion~
+})
+}
+)
