e87d90a5921a44ec40da94e9d11aa1afd4b86cea
aaqib-yfz <aaqibshaheer2001@gmail.com>
2024-08-17 16:51:58 +0500

    successful test ong

--- cypress/e2e/locators.cy.js
+++ cypress/e2e/locators.cy.js
@@ -1,7 +1,8 @@
-const cypress = require("cypress")
 describe('suite', ()=>{
 it( 'locs test', ()=>{
-    cy.visit()
+    cy.visit("https://www.daraz.pk/")
+    cy.get('input.search-box__input--O34g[ name = \'q\']').type("T-Shirts")
+    cy.get('a.search-box__button--1oH7').click()
 })
 }
 )
