b4284affab369a584069ec048438886af9b8c351
aaqib-yfz <aaqibshaheer2001@gmail.com>
2024-08-18 22:32:49 +0500

    exp assert

--- cypress/e2e/assertion.cy.js
+++ cypress/e2e/assertion.cy.js
@@ -12,7 +12,19 @@ describe('Assertion',()=>{
         
         cy.url().should('include','orangehrmlive.com').and('eq','https://opensource-demo.orangehrmlive.com/web/index.php/auth/login').and('contain','orangehrm').and('not.contain','greenhrm')
 
+        cy.title().should('include','Orange').and('eq','OrangeHRM')
+        cy.get('.orangehrm-login-branding > img').should('be.visible').and('exist')
 
+        cy.xpath('//a').should('have.length','5') //no of links
+        cy.get('.oxd-input.oxd-input--active[ name = \'username\']').type('Admin').should('have.value','Admin')
     })
+
+    it('Explicit Assertions',()=>{
+        cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+        cy.get('.oxd-input.oxd-input--active[ name = \'username\']').type('Admin')
+        cy.get('input.oxd-input.oxd-input--active[name = \'password\']').type('admin123')
+        cy.get('button.oxd-button.oxd-button--medium.oxd-button--main.orangehrm-login-button').click()
+    })
+
 }
 )
