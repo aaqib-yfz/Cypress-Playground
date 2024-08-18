807f59cf260abdac98aadc57b2f4f66fc088bee0
aaqib-yfz <aaqibshaheer2001@gmail.com>
2024-08-18 17:40:15 +0500

    Assertions

---
+++ cypress/e2e/assertion.cy.js
@@ -0,0 +1,18 @@
+
+describe('Assertion',()=>{
+    it('Implicit Assertion',()=>{
+        cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+        // should and 
+        
+        // cy.url().should('include','orangehrmlive.com')
+        // cy.url().should('eq','https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+        // cy.url().should('contain','orangehrm')
+
+        // cy.url().should('include','orangehrmlive.com').should('eq','https://opensource-demo.orangehrmlive.com/web/index.php/auth/login').should('contain','orangehrm')
+        
+        cy.url().should('include','orangehrmlive.com').and('eq','https://opensource-demo.orangehrmlive.com/web/index.php/auth/login').and('contain','orangehrm').and('not.contain','greenhrm')
+
+
+    })
+}
+)
