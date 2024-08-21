dc0cd7c0bfc8bace0d731323ad9be11a9234a74a
aaqib-yfz <aaqibshaheer2001@gmail.com>
2024-08-22 00:03:46 +0500

    list

--- cypress/e2e/drpdwns.cy.js
+++ cypress/e2e/drpdwns.cy.js
@@ -4,6 +4,6 @@ describe(' handling dropdowns uwu',()=>{
     it('with select',()=>{
         cy.visit('https://www.zoho.com/commerce/free-demo.html')
         cy.get('select#zcf_address_country').select('Italy').should('have.value','Italy')
-        cy.get('.chk_bx').check().should('be.checked')
+        cy.get('.chk_bx').check().should('be.checked').and()
     })
 })
