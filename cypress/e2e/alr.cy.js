1a04038a03f27fc121c3a92aee22f8adb75a10a7
aaqib-yfz <aaqibshaheer2001@gmail.com>
2024-08-25 18:09:21 +0500

    alert

---
+++ cypress/e2e/alr.cy.js
@@ -0,0 +1,11 @@
+
+///  <reference types = "cypress"/>
+
+
+// Alert1 =  text +  ok button
+// confirm Alert = text + ok & cancel btn
+// prompt Alert =  text with text box for user input and ok btn
+describe('Alert',()=>{
+    it('')
+}
+)
