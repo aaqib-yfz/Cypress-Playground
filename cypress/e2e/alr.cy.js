813ac7c0a8f0885194a82fea9342c16fd8229169
aaqib-yfz <aaqibshaheer2001@gmail.com>
2024-08-26 20:43:29 +0500

    alr last

--- cypress/e2e/alr.cy.js
+++ cypress/e2e/alr.cy.js
@@ -34,15 +34,42 @@ describe('Alert',()=>{
         cy.get('#result').should('have.text','You clicked: Ok')
     })
 
-    it.only('confirmation alert - cancel btn',()=>{
+    it('confirmation alert - cancel btn',()=>{
         cy.visit('https://the-internet.herokuapp.com/javascript_alerts')
         cy.get('button[ onclick =\'jsConfirm()\']').click();
 
         cy.on('window:confirm',(text)=>{
             expect(text).to.contain('I am a JS Confirm')
         })
-        cy.on('window:confirm',()=>false);
+        cy.on('window:confirm',()=>false); // close using cancel btn
         cy.get('#result').should('have.text','You clicked: Cancel')
     })
+
+    //prompt
+    it('Prompt Alert',()=>{
+        cy.visit('https://the-internet.herokuapp.com/javascript_alerts')
+        cy.window().then((win)=>{
+            cy.stub(win,'prompt').returns('Hello g')
+        })
+        cy.get('button[ onclick =\'jsPrompt()\']').click();
+        cy.wait(3000)
+        cy.get('#result').should('have.text','You entered: Hello g')
+
+    })
+
+    it.only('Authentication Alr',()=>{
+
+
+        // cy.visit('https://the-internet.herokuapp.com/basic_auth',{ 
+        //     auth:{
+        //         username:'admnin',
+        //         password:'admin'
+        //     },
+
+        // });
+        cy.visit('https://admin:admin@the-internet.herokuapp.com/basic_auth');
+        cy.wait(3000)
+        cy.get('div[class = \'example\'] p').contains('Congratulations! You must have the proper credentials.')
+    })    
 }
 )
