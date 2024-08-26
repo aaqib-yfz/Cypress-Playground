5d9f6b42a8c77a30d93463ae742d199eec1b2cfe
aaqib-yfz <aaqibshaheer2001@gmail.com>
2024-08-26 15:12:21 +0500

    al2

--- cypress/e2e/alr.cy.js
+++ cypress/e2e/alr.cy.js
@@ -5,7 +5,20 @@
 // Alert1 =  text +  ok button
 // confirm Alert = text + ok & cancel btn
 // prompt Alert =  text with text box for user input and ok btn
+// authenticated alert
+
+
 describe('Alert',()=>{
-    it('')
+    //simple alr
+    it('normal alrt', ()=>{
+        cy.visit('https://the-internet.herokuapp.com/javascript_alerts')
+        cy.get('button[ onclick =\'jsAlert()\']').click();
+        
+        cy.on('window:alert',(text)=>{
+            expect(text).to.contain('I am a JS Alert')
+        })
+        //alert automatically closed by cypress
+        cy.get('#result').should('have.text','You successfully clicked an alert')
+    })
 }
 )
