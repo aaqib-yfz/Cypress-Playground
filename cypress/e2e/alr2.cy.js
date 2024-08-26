ed1bda727c298476715d56fcf31361ff6e38da52
aaqib-yfz <aaqibshaheer2001@gmail.com>
2024-08-26 23:54:25 +0500

    alr2 first

---
+++ cypress/e2e/alr2.cy.js
@@ -0,0 +1,10 @@
+// Alert1 =  text +  ok button
+// confirm Alert = text + ok & cancel btn
+// prompt Alert =  text with text box for user input and ok btn
+// authenticated alert
+
+describe('Alerts Prac',()=>{
+    it('simple alert',()=>{
+        cy.visit('https://the-internet.herokuapp.com/javascript_alerts');
+    })
+})
