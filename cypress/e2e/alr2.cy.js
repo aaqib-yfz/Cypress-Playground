0cbfa6f8411d1fae5b2299a0cd2127d896ef0ebe
aaqib-yfz <aaqibshaheer2001@gmail.com>
2024-08-28 16:43:04 +0500

    alr p

--- cypress/e2e/alr2.cy.js
+++ cypress/e2e/alr2.cy.js
@@ -6,5 +6,26 @@
 describe('Alerts Prac',()=>{
     it('simple alert',()=>{
         cy.visit('https://the-internet.herokuapp.com/javascript_alerts');
+        cy.get('button[onClick = \'jsAlert()\']').click()
+
+    cy.on('window:alert',(text)=>{
+        expect(text).to.contain('I am a JS Alert')
+    })
+    })
+
+    it('confirm Alert',()=>{
+        cy.visit('https://the-internet.herokuapp.com/javascript_alerts');
+        cy.get('button[onClick = \'jsConfirm()\']').click();
+
+        cy.on('window:confirm',(text)=>{
+            expect(text).to.contain('I am a JS Confirm');
+        });
+    });
+
+    it.only('confirm cancel',()=>{
+        cy.visit('https://the-internet.herokuapp.com/javascript_alerts');
+        cy.get('button[onClick = \'jsConfirm()\']').click();
+        cy.on('window:confirm',()=>false);
+        cy.get('#result').should('have.text','You clicked: Cancel');
     })
 })
