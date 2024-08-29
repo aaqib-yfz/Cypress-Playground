1bd410c35382e525d8eb7b7e1cf228272ca1d657
aaqib-yfz <aaqibshaheer2001@gmail.com>
2024-08-30 00:39:50 +0500

    ap on ended

--- cypress/e2e/tabs2.cy.js
+++ cypress/e2e/tabs2.cy.js
@@ -2,5 +2,7 @@ describe('tb prc',()=>{
     it('ap one',()=>{
         cy.visit('https://the-internet.herokuapp.com/windows');
         cy.get('.example>a').invoke('removeAttr','target').click();
+        cy.url().should('include','https://the-internet.herokuapp.com/windows/new');
+        cy.go('back');
     })
 })
