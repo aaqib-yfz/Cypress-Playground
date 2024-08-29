1fc0473c46389766340c9e8e4c9c5ebc1b08d7e5
aaqib-yfz <aaqibshaheer2001@gmail.com>
2024-08-30 00:21:48 +0500

    tb last

--- cypress/e2e/tabs.cy.js
+++ cypress/e2e/tabs.cy.js
@@ -3,6 +3,16 @@ describe('tabs',()=>{
         cy.visit('https://the-internet.herokuapp.com/windows')
         cy.get('.example>a').invoke('removeAttr','target').click();
         cy.url().should('include','https://the-internet.herokuapp.com/windows/new');
+        cy.wait(3000)
+        cy.go('back');
+    })
+
+    it('ap2',()=>{
+        cy.visit('https://the-internet.herokuapp.com/windows')
+        cy.get('.example>a').then((e)=>{
+            let url = e.prop('href');
+            cy.visit(url);
+        })
         cy.go('back');
     })
 }) 
