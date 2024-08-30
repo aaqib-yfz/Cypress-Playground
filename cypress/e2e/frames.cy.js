1052d7c85ef85f327c540fd3d2bd3e990cca8aac
aaqib-yfz <aaqibshaheer2001@gmail.com>
2024-08-31 00:22:04 +0500

    frames 2, im tired

--- cypress/e2e/frames.cy.js
+++ cypress/e2e/frames.cy.js
@@ -1,5 +1,17 @@
+///  <reference types = "cypress"/>
+
 describe(' handling frames',()=>{
     it('one app',()=>{
-        cy.visit('https://the-internet.herokuapp.com/iframe')
+        cy.visit('https://www.w3schools.com/tags/tryit.asp?filename=tryhtml_iframe');
+        cy.get('#iframeResult')
+            .its('0.contentDocument.body')
+            .then((body) => {
+        cy.wrap(body)
+        .find("#tnb-google-search-mobile-show")
+        .should("be.visible")
+        //input#tnb-google-search-input
+
     })
+
+})
 })
