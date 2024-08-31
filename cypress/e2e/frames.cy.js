5dae4e7681953b448de0cd6f5f478c1cda65a387
aaqib-yfz <aaqibshaheer2001@gmail.com>
2024-08-31 21:23:24 +0500

    done

--- cypress/e2e/frames.cy.js
+++ cypress/e2e/frames.cy.js
@@ -2,16 +2,14 @@
 
 describe(' handling frames',()=>{
     it('one app',()=>{
-        cy.visit('https://www.w3schools.com/tags/tryit.asp?filename=tryhtml_iframe');
-        cy.get('#iframeResult')
+        cy.visit('https://www.w3schools.com/html/html_iframe.asp');
+        const iframe = cy.get('#mce_0_ifr')
             .its('0.contentDocument.body')
-            .then((body) => {
-        cy.wrap(body)
-        .find("#tnb-google-search-mobile-show")
-        .should("be.visible")
-        //input#tnb-google-search-input
+            .should('be.visible')
+            .then(cy.wrap);
 
+            iframe.clear().type('welcome')
     })
 
 })
-})
+
